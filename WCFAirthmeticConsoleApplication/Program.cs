978471ae7b15b9c2ae8ff63dfb0915780a62e3e1
Suhail Mushtaq <suhailmushtaq.dev@gmail.com>
2019-03-01 16:00:36 +0530

    first chkin

---
+++ WCFAirthmeticConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFAirthmeticConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating Proxy for the MyService 
+            Console.WriteLine("Client calling the service...");
+            Console.WriteLine("Hello Ram");
+            Console.Read();
+        }
+    }
+}
